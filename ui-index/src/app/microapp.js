4ff54ac721c3111369e0e387bd3269d37d6cf80c
Jagadeesh <jagadeeshthegeek@gmail.com>
2021-06-05 18:35:33 -0700

    updated: onProxyRes

--- ui-index/src/app/microapp.js
+++ ui-index/src/app/microapp.js
@@ -2,6 +2,7 @@ const {
   createProxyMiddleware,
   responseInterceptor,
 } = require("http-proxy-middleware");
+const zlib = require("zlib");
 
 const navs = [
   { title: "App1", url: "/app1" },
@@ -43,7 +44,7 @@ const validateMicroApp = function (req, res, next) {
   }
 };
 
-const onProxyRes = (proxyRes, req, res) => {
+const onProxyRes1 = (proxyRes, req, res) => {
   const bodyChunks = [];
   proxyRes.on("data", (chunk) => {
     bodyChunks.push(chunk);
@@ -51,7 +52,8 @@ const onProxyRes = (proxyRes, req, res) => {
   proxyRes.on("end", () => {
     const body = Buffer.concat(bodyChunks);
     let microAppResp = body.toString("utf8");
-    console.log("::end"); // log response body
+    console.log("::end");
+    console.log(microAppResp);
     res.render("pages/microapp", {
       microAppResp,
       navs,
@@ -60,18 +62,95 @@ const onProxyRes = (proxyRes, req, res) => {
   });
 };
 
+const onProxyRes2 = responseInterceptor(
+  async (responseBuffer, proxyRes, req, res) => {
+    // log original request and proxied request info
+    const exchange = `[DEBUG] ${req.method} ${req.path} -> ${proxyRes.req.protocol}//${proxyRes.req.host}${proxyRes.req.path} [${proxyRes.statusCode}]`;
+    console.log(exchange); // [DEBUG] GET / -> http://www.example.com [200]
+
+    // log complete response
+    const response = responseBuffer.toString("utf8");
+    console.log(response); // log response body
+
+    return response;
+  }
+);
+
+const onProxyRes3 = async (proxyRes, req, res) => {
+  res.end = (data) => {
+    console.log("##############::end4");
+  };
+  onProxyRes2(proxyRes, req, res);
+};
+
+/**
+ * Streaming decompression of proxy response
+ * source: https://github.com/apache/superset/blob/9773aba522e957ed9423045ca153219638a85d2f/superset-frontend/webpack.proxy-config.js#L116
+ */
+function decompress(proxyRes, contentEncoding) {
+  let _proxyRes = proxyRes;
+  let decompress;
+  switch (contentEncoding) {
+    case "gzip":
+      decompress = zlib.createGunzip();
+      break;
+    case "br":
+      decompress = zlib.createBrotliDecompress();
+      break;
+    case "deflate":
+      decompress = zlib.createInflate();
+      break;
+    default:
+      break;
+  }
+  if (decompress) {
+    _proxyRes.pipe(decompress);
+    _proxyRes = decompress;
+  }
+  return _proxyRes;
+}
+
+function onProxyRes(proxyRes, req, res) {
+  const originalProxyRes = proxyRes;
+  let buffer = Buffer.from("", "utf8");
+  // decompress proxy response
+  const _proxyRes = decompress(proxyRes, proxyRes.headers["content-encoding"]);
+  // concat data stream
+  _proxyRes.on("data", (chunk) => (buffer = Buffer.concat([buffer, chunk])));
+  _proxyRes.on("end", () => {
+    const responseBuffer = Buffer.from(buffer);
+    const microAppResp = responseBuffer.toString("utf8");
+    console.log("###end####");
+    console.log(microAppResp);
+    res.render("pages/microapp", {
+      microAppResp,
+      navs,
+      title: "Microapp: " + req.params.appId,
+    });
+  });
+  _proxyRes.on("error", (error) => {
+    res.end(`Error fetching proxied request: ${error.message}`);
+  });
+}
+
 const options1 = {
   // target: POSTS_API,
   changeOrigin: true,
   proxyTimeout: 5000,
+  // pathRewrite: function (path, req) {
+  //   return path.replace("/app1", "/");
+  // },
   router: getMicroAppUrl,
   selfHandleResponse: true, // selfHandleResponse=true (prevent automatic call of res.end())
-  onProxyRes,
+  onProxyRes: onProxyRes,
 };
 
 const options2 = {
   changeOrigin: true,
   proxyTimeout: 5000,
+  // pathRewrite: function (path, req) {
+  //   return path.replace("/app1", "/");
+  // },
   router: getMicroAppUrl,
 };
 

70a8923efd62e2fd7f6c58ec5aae5e8729e58862
Jagadeesh <jagadeeshthegeek@gmail.com>
2022-06-26 21:30:07 -0700

    added sysAdmin

--- x-data/x-generate/index.js
+++ x-data/x-generate/index.js
@@ -5,6 +5,7 @@ const { main: generateUserGroups } = require("./userGroups");
 const { main: generateStudents } = require("./students");
 const { main: generateTeachers } = require("./teachers");
 const { main: generateTenantAdmins } = require("./tenantAdmins");
+const { main: generateSysAdmins } = require("./sysAdmins");
 
 const { main: generateCourses } = require("./courses");
 const { main: generateCourseGroups } = require("./courseGroups");
@@ -16,6 +17,7 @@ generateUserGroups();
 generateStudents(); // get all users where userGroup=studentUserGroupId1
 generateTeachers(); // get all users where userGroup=teacherUserGroupId1
 generateTenantAdmins(); // get all users where userGroup=tenantAdminUserGroupId1
+generateSysAdmins(); // get all users where userGroup=sysAdminUserGroupId1
 
 generateCourses();
 generateCourseGroups();
---
+++ x-data/x-generate/sysAdmins.js
@@ -0,0 +1,55 @@
+const { generateFile, dirPrefix, viewer } = require("./utils");
+
+const meta = {
+  pageSize: 10,
+  pageNo: 1,
+  nextPage: "nextPageCursor",
+  prevPage: "prevPageCursor",
+  access: [],
+};
+
+function main() {
+  // sysAdmins:details (viewer: sysAdmin)
+  generateFile({
+    noOfItems: 1,
+    isDetail: true,
+    filePath: `${dirPrefix}/${viewer.sysAdminViewer}/sysAdmins/get/details/res.json`,
+    meta,
+    idPrefix: "userId",
+    namePrefix: "System Admin User",
+    itemMeta: {
+      access: [
+        "READ_SYS_ADMIN_USER",
+        "UPDATE_SYS_ADMIN_USER",
+        "DELETE_SYS_ADMIN_USER",
+      ],
+    },
+    itemData: {
+      userGroupIds: ["sysAdminUserGroupId1", "userGroupId2", "userGroupId3"],
+    },
+  });
+
+  // sysAdmins:list (viewer: sysAdmin)
+  generateFile({
+    noOfItems: 10,
+    filePath: `${dirPrefix}/${viewer.sysAdminViewer}/sysAdmins/get/list/res.json`,
+    meta: {
+      ...meta,
+      access: ["CREATE_SYS_ADMIN_USER"],
+    },
+    idPrefix: "userId",
+    namePrefix: "System Admin User",
+    itemMeta: {
+      access: [
+        "READ_SYS_ADMIN_USER",
+        "UPDATE_SYS_ADMIN_USER",
+        "DELETE_SYS_ADMIN_USER",
+      ],
+    },
+    itemData: {
+      userGroupIds: ["sysAdminUserGroupId1", "userGroupId2", "userGroupId3"],
+    },
+  });
+}
+
+module.exports = { main };
